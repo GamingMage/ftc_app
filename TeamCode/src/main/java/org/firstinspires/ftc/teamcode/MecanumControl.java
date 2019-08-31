adfbd31b556c03305939e793bd11c02142687d15
GamingMage <josephliang@roadrunner.com>
2019-08-31 14:50:52 -0400

    Finished mecanum control
    In record time

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumControl.java
@@ -24,6 +24,11 @@ public class MecanumControl extends OpMode
     }
     @Override
     public void loop() {
+        telemetry.addData("RSx",gamepad1.right_stick_x);
+        telemetry.addData("LSy",gamepad1.left_stick_y);
+        telemetry.addData("LSx",gamepad1.left_stick_x);
+        telemetry.update();
+
         //Set everything to zero when neither stick is in use
         if (gamepad1.left_stick_y==0 && gamepad1.left_stick_x==0 && gamepad1.right_stick_x==0){
             robot.rightBack.setPower(0);
@@ -41,10 +46,49 @@ public class MecanumControl extends OpMode
         }
 
         //Control of forward and backward movement (Kylie)
+        if (gamepad1.left_stick_x==0 && gamepad1.left_stick_y!=0) {
+            robot.rightBack.setPower(-gamepad1.left_stick_y);
+            robot.leftBack.setPower(-gamepad1.left_stick_y);
+            robot.rightFront.setPower(-gamepad1.left_stick_y);
+            robot.leftFront.setPower(-gamepad1.left_stick_y);
+        }
 
         //Control of diagonal movement (S!am)
+        //Quadrant i
+        if (gamepad1.left_stick_x>0 && gamepad1.left_stick_y<0){
+            robot.leftFront.setPower(gamepad1.left_stick_x);
+            robot.rightBack.setPower(gamepad1.left_stick_x);
+            robot.leftBack.setPower(0);
+            robot.rightFront.setPower(0);
+        }
+        //Quadrant ii
+        if (gamepad1.left_stick_x<0 && gamepad1.left_stick_y<0){
+            robot.leftFront.setPower(0);
+            robot.rightBack.setPower(0);
+            robot.leftBack.setPower(-gamepad1.left_stick_y);
+            robot.rightFront.setPower(-gamepad1.left_stick_y);
+        }
+        //Quadrant iii
+        if (gamepad1.left_stick_x<0 && gamepad1.left_stick_y>0){
+            robot.leftFront.setPower(gamepad1.left_stick_x);
+            robot.rightBack.setPower(gamepad1.left_stick_x);
+            robot.leftBack.setPower(0);
+            robot.rightFront.setPower(0);
+        }
+        //Quadrant iv
+        if (gamepad1.left_stick_x>0 && gamepad1.left_stick_y>0){
+            robot.leftFront.setPower(0);
+            robot.rightBack.setPower(0);
+            robot.leftBack.setPower(-gamepad1.left_stick_y);
+            robot.rightFront.setPower(-gamepad1.left_stick_y);
+        }
 
         //Control of left and right turning (Perspective shift) (Kevin)
-
+        if (gamepad1.right_stick_x!=0){
+            robot.rightBack.setPower(-gamepad1.right_stick_x);
+            robot.leftFront.setPower(gamepad1.right_stick_x);
+            robot.rightFront.setPower(-gamepad1.right_stick_x);
+            robot.leftBack.setPower(gamepad1.right_stick_x);
+        }
     }
 }
