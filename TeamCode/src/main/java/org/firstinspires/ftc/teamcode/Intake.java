a68db8d8ec2aa2ec362010ab91892e782d192325
GamingMage <josephliang@roadrunner.com>
2019-11-09 14:49:15 -0500

    Code Structure
    New classes for this year's code with outline of what each will include

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Intake {
+
+    //This class will contain the 2 motors that drive the intake system
+    //sensors include a touch sensor to stop system when sensor is hit by block (grab block when hit)
+    //Methods will include an in/off/reverse
+}
